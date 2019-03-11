ba7e7640ec6352f4973305c87f3adb3b9c37ff32
Aung Kyaw Kyaw Myo <aungkyawkyawmyo@marathonmyanmar.com>
2019-03-11 16:55:06 +0630

    part 3 - 9

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
-    <Header />
+    <!-- <Header /> -->
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSidenav
+      :show="displaySidenav"
+      @close="displaySidenav = false" />
     <nuxt />
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header'
+// import Header from '@/components/Header'
+import TheHeader from '@/components/Navigation/TheHeader'
+import TheSidenav from '@/components/Navigation/TheSidenav'
+import Header from '@/components/Navigation/TheHeader'
 
 export default {
   components: {
-    Header
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySidenav: false
+    }
   }
 }
 </script>
 
 <style>
+html {
+  font-family: 'Open Sans', sans-serif
+}
+
+body {
+  margin: 0;
+}
 </style>
