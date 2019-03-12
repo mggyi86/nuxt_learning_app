635c7065fabb0143e7c7e3452e3a3b9737e43c95
Aung Kyaw Kyaw Myo <aungkyawkyawmyo@marathonmyanmar.com>
2019-03-12 17:39:47 +0630

    add part 3

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,16 +2,19 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hello there!"
       previewText="This is my first post!" />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hello there - the second time!"
       previewText="This is my second post!" />
     <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hi!"
       previewText="This is my third post!" />
@@ -24,6 +27,12 @@ import PostPreview from '@/components/Posts/PostPreview'
 export default {
   components: {
     PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   }
 }
 </script>
